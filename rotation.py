b4934d5fc586807d4f4545551230281647ef147b
Vybekid <felobitto@gmail.com>
2025-07-12 13:06:40 +0300

    submission 1

--- rotation.py
+++ rotation.py
@@ -1,20 +1,60 @@
-import turtle 
-import random 
+import turtle
 
+# Set up the screen
 screen = turtle.Screen()
-screen.bgcolor("black")
+screen.bgcolor("white")
 
+# Create the turtle
 t = turtle.Turtle()
 t.speed(0)
+t.pensize(2)
 
-colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+def draw_wing(color, size, angle):
+    """Draws one wing of the butterfly."""
+    t.color(color)
+    t.begin_fill()
+    t.left(angle)
+    t.forward(size)
+    for _ in range(2):
+        t.right(90)
+        t.forward(size)
+    t.right(90)
+    t.forward(size)
+    t.end_fill()
 
-for i in range(200): 
-    t.pencolor(colors[i % 6])
-    t.width(i / 100 + 1)
-    t.forward(i)
-    t.left(59)
+def main():
+    """Main function to draw the butterfly."""
+    t.penup()
+    t.goto(0, -100)
+    t.pendown()
+    t.setheading(90)
 
-t.hideturtle()
+    # Draw the wings
+    draw_wing("orange", 100, 45)
+    draw_wing("yellow", 80, 45)
+    t.setheading(270)
+    draw_wing("orange", 100, 45)
+    draw_wing("yellow", 80, 45)
 
-t.done()
+    # Draw the body
+    t.penup()
+    t.goto(0, -100)
+    t.pendown()
+    t.color("black")
+    t.setheading(90)
+    t.forward(200)
+
+    # Draw the antennae
+    t.left(30)
+    t.forward(50)
+    t.penup()
+    t.goto(0, 100)
+    t.pendown()
+    t.setheading(150)
+    t.forward(50)
+
+    t.hideturtle()
+    turtle.done()
+
+if __name__ == "__main__":
+    main()
